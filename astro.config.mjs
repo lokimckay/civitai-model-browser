53fc34782351a1c2337ad4db0a2f9af9740600e0
Loki <lokimckay@gmail.com>
2024-06-26 13:07:09 +1000

    remove unneeded worker integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-import worker from "@astropub/worker";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lokimckay.github.io",
   base: "civitai-model-browser",
-  integrations: [preact(), worker()],
+  integrations: [preact()],
 });
