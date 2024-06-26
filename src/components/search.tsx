501e02149ca8d3cdead1cf5e7b4b0644079a5584
Loki <lokimckay@gmail.com>
2024-06-26 23:55:14 +1000

    debounce search input

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,10 +1,21 @@
 import { useStore } from "@nanostores/preact";
 import { $search } from "@/lib/store";
 import Input from "./input";
+import debounce from "lodash.debounce";
 import "./search.css";
+import { useMemo } from "preact/hooks";
 
 export default function Search() {
   const search = useStore($search);
+
+  const onInput = useMemo(
+    () =>
+      debounce((event: Event) => {
+        $search.set((event.target as HTMLInputElement).value);
+      }, 300),
+    []
+  );
+
   return (
     <>
       <Input
@@ -12,9 +23,7 @@ export default function Search() {
         class="search"
         placeholder="Search"
         value={search}
-        onInput={(event) => {
-          $search.set(event.currentTarget.value);
-        }}
+        onInput={onInput}
       />
     </>
   );
