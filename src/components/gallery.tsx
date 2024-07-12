95721443f4fff198d4854e789e1f67459ff65f97
Loki <lokimckay@gmail.com>
2024-07-12 21:57:29 +1000

    show image metadata and fix gallery arrows

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,7 +1,8 @@
-import { useEffect, type HTMLAttributes } from "preact/compat";
-import { useSignal } from "@preact/signals";
-import "./gallery.css";
+import { useCallback, useEffect, type HTMLAttributes } from "preact/compat";
+import { useSignal, computed } from "@preact/signals";
 import { updateModel } from "@/lib/store";
+import exifr from "exifr";
+import "./gallery.css";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   id: string;
@@ -12,7 +13,44 @@ export default function Gallery(props: Props) {
   const { id, mvId, className, children, ...rest } = props;
   const _children = Array.isArray(children) ? children : [children];
   const activeIdx = useSignal<number | null>(null);
+  const activeChild = computed(() =>
+    activeIdx.value !== null ? _children[activeIdx.value] : null
+  );
+
+  const metadata = useSignal<any | null>(null);
+  const userComment = useSignal<string | null>(null);
   const isLightboxActive = activeIdx.value !== null;
+
+  const getTags = useCallback(async () => {
+    if (!activeChild.value) return;
+    const url = activeChild.value.props.src;
+    const tags = await exifr.parse(url, true);
+    metadata.value = tags;
+
+    const arr = metadata.value?.["userComment"];
+    const decoded = arr && new TextDecoder().decode(arr);
+    const pruned = decoded?.replace(/[^ -~]+/g, "").replace("UNICODE", "");
+    const split = pruned
+      ?.split("Negative prompt: ")
+      .join("\n\nNegative prompt: ")
+      .split("Steps: ")
+      .join("\n\nSteps: ")
+      .split("Sampler: ")
+      .join("\nSampler: ")
+      .split("CFG scale: ")
+      .join("\nCFG scale: ")
+      .split("Seed: ")
+      .join("\nSeed: ")
+      .split("Size: ")
+      .join("\nSize: ")
+      .split("Model hash: ")
+      .join("\nModel hash: ");
+    userComment.value = arr ? split : null;
+  }, [activeChild.value, isLightboxActive]);
+
+  useEffect(() => {
+    getTags();
+  }, [getTags]);
 
   useEffect(() => {
     const handleKeydown = (event: KeyboardEvent) => {
@@ -53,12 +91,17 @@ export default function Gallery(props: Props) {
     <>
       {isLightboxActive && (
         <div id={`${id}-lightbox`} class="lightbox" onClick={hideLightbox}>
-          <button onClick={nextImg}>←</button>
-          {_children[activeIdx.value!]}
-          <button onClick={prevImg}>→</button>
+          <button onClick={prevImg}>←</button>
+          {activeChild}
+          <button onClick={nextImg}>→</button>
           <button class="set-thumbnail" onClick={setThumbnail}>
             Set thumbnail
           </button>
+          {userComment.value && (
+            <div class="metadata">
+              <textarea readOnly>{userComment.value}</textarea>
+            </div>
+          )}
         </div>
       )}
       <div id={`${id}-gallery`} class={`gallery ${className}`} {...rest}>
